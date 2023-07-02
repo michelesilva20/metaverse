f378620d08e247ec317ca20a58b2c847baa73ca5
michelesilva20 <michele.silva0511@gmail.com>
2023-07-02 17:41:19 -0300

    five-day: feito seção feedback e footer
    finalização do projeto

--- app/page.js
+++ app/page.js
@@ -16,14 +16,12 @@ const Page = () => (
       <div className='gradient-04 z-0'/>
       <WhatsNew />
     </div>
-
+    <World />
     <div className='relative'>
-      <World />
-      <div className='gradient-04 z-0'/>
       <Insights />
+      <div className='gradient-04 z-0'/>
+      <Feedback />
     </div>
-
-    <Feedback />
     <Footer />
   </div>
 );
